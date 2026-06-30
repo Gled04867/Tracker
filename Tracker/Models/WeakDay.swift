8e6c37c12abaf87b20f97f360c1db4ed0449e491
Gled04867 <Gled04867@ya.ru>
2026-06-30 10:47:54 +0300

    style(weekday): убрать избыточные raw values
    Оставить raw value только у первого case, остальные Swift
    проставит автоматически по порядку

--- Tracker/Models/WeakDay.swift
+++ Tracker/Models/WeakDay.swift
@@ -2,12 +2,12 @@ import Foundation
 
 enum WeekDay: Int, CaseIterable, Codable {
     case sunday = 1
-    case monday = 2
-    case tuesday = 3
-    case wednesday = 4
-    case thursday = 5
-    case friday = 6
-    case saturday = 7
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
     
     var title: String {
         switch self {
